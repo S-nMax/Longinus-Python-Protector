beefde2e42e91c5029a06ccccb09be1c8c1501bf
When tomorrow comes <yinxizixi@outlook.com>
2026-04-05 09:17:41 +0800

    Update and rename longinus.py to Longinus.py

--- Longinus.py
+++ Longinus.py
@@ -1,4 +1,28 @@
-# Longinus
+/*
+ * [Longinus.py]
+ * [A Python code protector based on a pure Python Marshal bytecode protection scheme]
+ * 
+ * Copyright (C) [2026] [When tomorrow comes/Mistpine Studio]
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+"""
+Longinus
+
+* Currently only available in Chinese
+"""
 
 import os
 import base64
